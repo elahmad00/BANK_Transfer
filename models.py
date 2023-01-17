ba655fa8437a505235b33fe2f86b0ae8750c9463
ahmad muhammad <elahmad00@gmail.com>
2023-01-17 17:24:47 +0100

    writing models

--- models.py
+++ models.py
@@ -1,1 +1,65 @@
 from database import Base
+from sqlalchemy import Column,String,Integer,ForeignKey
+from sqlalchemy.orm import relationship
+
+
+
+class BlogModel(Base):
+    __tablename__ = 'Blog'
+
+    id = Column(Integer,primary_key=True,index=True)
+    title = Column(String)
+    body = Column(String)
+    user_id = Column(Integer,ForeignKey('Users.id'))
+    owner = relationship("UserModel",back_populates="blogs")
+
+
+class UserModel(Base):
+    __tablename__ = 'Users'
+
+    id = Column(Integer,primary_key=True,index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String,unique=True)
+    phoneNumber = Column(String)
+    password = Column(String) 
+
+    blogs = relationship("BlogModel",back_populates="owner")
+
+
+class Banks(Base):
+    __tablename__ = 'banks'
+    id = Column(Integer,primary_key=True,index=True)
+    bank_id = Column(Integer)
+    code = Column(String)
+    name = Column(String)
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+    id = Column(Integer,primary_key=True,index=True)
+    user_id = Column(Integer,ForeignKey('Users.id'))
+    customer_id = Column(String)
+
+
+class AccountRef(Base):
+    __tablename__ = 'account_ref'
+    id = Column(Integer,primary_key=True,index=True)
+    user_id = Column(Integer,ForeignKey('Users.id'))
+    accountReference = Column(String)
+
+
+class UserReservedAccount(Base):
+    __tablename__ = "user_accounts"
+    id = Column(Integer,primary_key=True,index=True)
+    user_id = Column(Integer,ForeignKey('Users.id'))
+    bank_code = Column(String)
+    bank_name = Column(String)
+    AccountNumber = Column(String)
+    AccountName = Column(String)
+
+
+
+
+
+
