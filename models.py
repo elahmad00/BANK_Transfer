9047b8b40299e726a1cfa138a897a186cc8d70c9
ahmad muhammad <elahmad00@gmail.com>
2023-01-17 17:26:06 +0100

    modelling the bankAPI

--- models.py
+++ models.py
@@ -3,30 +3,6 @@ from sqlalchemy import Column,String,Integer,ForeignKey
 from sqlalchemy.orm import relationship
 
 
-
-class BlogModel(Base):
-    __tablename__ = 'Blog'
-
-    id = Column(Integer,primary_key=True,index=True)
-    title = Column(String)
-    body = Column(String)
-    user_id = Column(Integer,ForeignKey('Users.id'))
-    owner = relationship("UserModel",back_populates="blogs")
-
-
-class UserModel(Base):
-    __tablename__ = 'Users'
-
-    id = Column(Integer,primary_key=True,index=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    email = Column(String,unique=True)
-    phoneNumber = Column(String)
-    password = Column(String) 
-
-    blogs = relationship("BlogModel",back_populates="owner")
-
-
 class Banks(Base):
     __tablename__ = 'banks'
     id = Column(Integer,primary_key=True,index=True)
